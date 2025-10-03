eca953aec315f89f00ca09a66ff0f5e52142ffc6
Jonablo <jonasanchez1993@gmail.com>
2025-10-03 11:04:33 -0500

    the basics nvim setup...

---
+++ init.lua
@@ -0,0 +1,11 @@
+-- theme & transparency
+vim.cmd.colorscheme("unokai")
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- Basic settings
+vim.opt.number = true                              -- Line numbers
+vim.opt.wrap = false                               -- Don't wrap lines
+vim.opt.scrolloff = 10                             -- Keep 10 lines above/below cursor
+vim.opt.sidescrolloff = 8                          -- Keep 8 columns left/right of cursor
