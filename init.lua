8f4446ba8083a8e45e08d1ec36d1206b4d337f39
Jonablo <jonasanchez1993@gmail.com>
2026-02-14 20:19:48 -0500

    feat(ui): sync terminal background color with neovim using OSC 11

--- init.lua
+++ init.lua
@@ -2,4 +2,22 @@ require("core.keymaps")
 require("core.plugins")
 require("core.plugins-setup")
 
-vim.o.columns = vim.env.COLUMNS
+vim.api.nvim_create_autocmd({ "UIEnter", "ColorScheme" }, {
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = "Normal" })
+    if not normal.bg then return end
+    
+    -- Envía un comando de escape OSC 11 para cambiar el color de fondo de la terminal
+    io.write(string.format("\027]11;#%06x\027\\", normal.bg))
+  end,
+})
+
+-- Opcional: Para restaurar el color de fondo original de la terminal al salir de Neovim
+vim.api.nvim_create_autocmd({ "VimLeave" }, {
+  callback = function()
+    
+      -- Envía un comando de escape OSC 111 para restaurar el color de fondo predeterminado
+    io.write("\027]111\027\\")
+  end,
+})
+
